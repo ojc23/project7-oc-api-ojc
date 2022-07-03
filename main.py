c30a9109ed76bee0eaec37beba96d7969fcdce38
Olivier OJC <85003518+ojc23@users.noreply.github.com>
2022-07-03 23:33:59 +0200

    json

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def predict():
 
     return {'proba_computed': str(probas[1])}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, workers=1)
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", port=8000, workers=1)
 
 
 
