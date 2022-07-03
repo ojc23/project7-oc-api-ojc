389045164295b42bc8a9a1b4fea7dce9442e0de7
Olivier OJC <85003518+ojc23@users.noreply.github.com>
2022-07-03 11:14:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,6 @@ def preditc():
 
 
 # http://localhost:5000/
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=5000)
 if __name__ == "__main__":
-    #app.run(debug=True)
-    uvicorn.run(app, host='127.0.0.1', port=8000)
