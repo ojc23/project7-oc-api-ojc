b883e657ebd0615e0044f36120c46fd9a86f17ae
Olivier OJC <85003518+ojc23@users.noreply.github.com>
2022-07-03 19:22:38 +0200

    Update apiOjc.py

--- apiOjc.py
+++ apiOjc.py
@@ -9,7 +9,7 @@ import pandas as pd
 # 1. to run use : python3 api.py 
 
 #app = Flask(__name__)
-app = FastAPI(__name__)
+app = FastAPI()
 
 # 2. Index route, opens automatically on http://127.0.0.1:5000
 @app.get('/')
