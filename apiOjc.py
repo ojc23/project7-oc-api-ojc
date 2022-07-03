18a58732200213c4e795e7c16c258c5b0cc6e1ce
Olivier OJC <85003518+ojc23@users.noreply.github.com>
2022-07-03 19:39:31 +0200

    Update apiOjc.py

--- apiOjc.py
+++ apiOjc.py
@@ -34,5 +34,7 @@ def predict_proba():
 
 # http://localhost:5000/
 if __name__ == '__main__':
-    uvicorn.run(app=app, host='127.0.0.1', port=5000)
+    #uvicorn.run(app=app, host='127.0.0.1', port=5000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+
 
