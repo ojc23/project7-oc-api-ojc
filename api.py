83aedfb4c9ee9bcc198363492e224da13e684336
Olivier OJC <85003518+ojc23@users.noreply.github.com>
2022-07-03 13:08:37 +0200

    Update api.py

--- api.py
+++ api.py
@@ -35,5 +35,6 @@ def preditc():
 
 # http://localhost:5000/
 if __name__ == '__main__':
-    #uvicorn.run(app, host='127.0.0.1', port=5000)
+    #uvicorn.run(app, host='127.0.0.1', port=8000)
     #uvicorn.run(app)
+    uvicorn api:app --reload
