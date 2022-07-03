294a3ce68a9d00647287b523918320ff4f58c67d
Olivier OJC <85003518+ojc23@users.noreply.github.com>
2022-07-03 19:05:14 +0200

    update

--- apiP.py
+++ apiP.py
@@ -2,32 +2,37 @@
 from flask import Flask
 import pickle
 import pandas as pd
-
+import ast
+import json
 
 app = Flask(__name__)
 
 # 3. Index route, opens automatically on http://127.0.0.1:8000
 @app.route('/')
 def index():
-    return {'message': 'Hello, API for Credit scoring'}
+    return {'message': 'Hello, API for Credit scoring ojc'}
 
-@app.route("/predit")
-def preditc():
+@app.route("/predict/")
+def predict():
     # load the model from disk
-    filename = r'../notebook/model.pkl'
+    filename = r'./model.pkl'
     model = pickle.load(open(filename, 'rb'))
 
     #Load Dataframe
-    path_df = '../notebook/x_test.csv'
+    path_df = './x_test.csv'
     #x_test = pd.read_csv(path_df, index_col=0, nrows=20)
-    x_test = pd.read_csv('../notebook/x_test.csv', nrows=100).set_index('SK_ID_CURR')
-    y_test = pd.read_csv('../notebook/x_test.csv', nrows=100).set_index('SK_ID_CURR')
+    x_test = pd.read_csv('./x_test.csv', nrows=100).set_index('SK_ID_CURR')
+    y_test = pd.read_csv('./y_test.csv', nrows=100).set_index('SK_ID_CURR')
         
     probas = model.predict_proba(x_test)[:,1]
+    dict_proba = {'proba_computed': str(probas[0])}
+    json_ = json.dumps(dict_proba)
 
-    return {'proba_failed': str(probas[0])}
+    print(json_)
+    #return json_
+    return dict_proba
 
 
 # http://localhost:5000/
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ api_test.py
@@ -0,0 +1,40 @@
+import requests
+import pandas as pd
+import requests, json
+import ast
+
+
+url = 'http://127.0.0.1:5000/predict/'
+
+#def prediction(self, id_client):
+def prediction():
+    #url ="https://fastapi-avi-oc-projet7.herokuapp.com/predict"
+    url ='http://127.0.0.1:5000/predict/'
+    #data_to_predict = self.data[self.data["SK_ID_CURR"] == id_client].drop(['SK_ID_CURR', 'TARGET'], axis = 1)
+    #data_to_predict = pd.DataFrame(self.data.drop(['SK_ID_CURR', 'TARGET'], axis = 1).iloc[id]).T
+    #data_to_predict_json = json.dumps(data_to_predict.to_dict('records')[0])
+    headers = {
+            'Content-Type': 'application/json'
+        }
+    #response = requests.request("POST", url, headers=headers, data=data_to_predict_json)
+    response = requests.request("POST", url, headers=headers)
+    #response = requests.post(url)
+    #proba = pd.read_json(response.json())
+    
+    #proba = float(ast.literal_eval(response.text)["proba_computed"])
+    
+    print(response)
+    
+    #if proba > 0.7:
+    #    message = "La probabilité de non remboursement est de: "
+    #    result = "Ce client est à risque"
+    #else:
+    #    message = "La probabilité de remboursement est de: "
+    #    result = "Client éligible au prêt"
+    #    proba = 1 - proba
+
+    #print (proba, result, message)
+    #return (proba, result, message)
+
+if __name__ == '__main__' :
+    prediction()
