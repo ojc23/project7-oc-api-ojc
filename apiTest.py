b997c7c2766bb37448531980c6112def0f805a21
Olivier OJC <85003518+ojc23@users.noreply.github.com>
2022-07-04 10:03:29 +0200

    Update apiTest.py

--- apiTest.py
+++ apiTest.py
@@ -6,7 +6,8 @@ import ast
 #def prediction(self, id_client):
 def prediction():
     #url ="https://ojc23-project7-oc-api-ojc-main-6h08ks.streamlitapp.com/predict"
-    url ="https://fastapi-avi-oc-projet7.herokuapp.com/predict"
+    url ="https://ojc23-project7-oc-api-ojc-main-6h08ks.streamlitapp.com"
+    #url ="https://fastapi-avi-oc-projet7.herokuapp.com/predict"
     #url ='http://127.0.0.1:8000/predict/'
     headers = {'Content-Type': 'application/json'}
 
